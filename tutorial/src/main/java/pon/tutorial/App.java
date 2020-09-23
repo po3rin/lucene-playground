81d1fca03d7d5cc027a7d06a0953b3b3fcbf561c
中村 弘武 <hiromu.nakamura@m3-2018mac32.local>
2020-09-23 22:30:41 +0900

    lucene tutorial

---
+++ tutorial/src/main/java/pon/tutorial/App.java
@@ -0,0 +1,13 @@
+package pon.tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
